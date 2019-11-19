c9e2af9b6c607b3bdc78ebab7e870b02d33a1ab9
Levin Weinstein <weinsteinlevin@gmail.com>
2019-11-18 23:48:36 -0800

    Add Judge with all solution options

--- Judge.java
+++ Judge.java
@@ -1,4 +1,6 @@
-import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.Map;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -6,15 +8,7 @@ public class Judge {
 
     private int[][] adjacencyMatrix;
 
-    public Judge(int peopleInTown, int [][] trust){
-        adjacencyMatrix = new int[peopleInTown][peopleInTown];
-
-        for (int[] edge : trust){
-            adjacencyMatrix[edge[0]][edge[1]] = 1;
-        }
-    }
-
-    public boolean trustsNoOne(int person){
+    private boolean trustsNoOne(int person){
         for (int i = 0; i < adjacencyMatrix.length; i++){
             if (i != person && adjacencyMatrix[person][i] != 0)
                 return false;
@@ -22,7 +16,7 @@ public class Judge {
         return true;
     }
 
-    public boolean everyoneTrusts(int person){
+    private boolean everyoneTrusts(int person){
         for (int i = 0; i < adjacencyMatrix.length; i++){
             if (i != person && adjacencyMatrix[i][person] == 0)
                 return false;
@@ -30,16 +24,73 @@ public class Judge {
         return true;
     }
 
-    public int findJudge(){
-        int firstSolution = -1;
 
-        for (int i = 0; i < adjacencyMatrix.length; i++){
-            if (trustsNoOne(i) && everyoneTrusts(i)){
+
+    public int findJudge(int peopleInTown, int [][] trust){
+        /* adjacencyMatrix = new int[peopleInTown][peopleInTown];
+
+        for (int[] edge : trust)
+            adjacencyMatrix[edge[0] - 1][edge[1] - 1] = 1;
+
+        int firstSolution = -1;
+        for (int person = 1; person <= adjacencyMatrix.length; person++){
+            int index = person - 1;
+
+            if (trustsNoOne(index) && everyoneTrusts(index)){
+                if (firstSolution != -1) return -1;
+                firstSolution = person;
+            }
+
+        }
+        return firstSolution;
+
+        */
+
+
+        /* Set<Integer> trusters = new HashSet<>();
+        Map<Integer, Integer> trusteesPerPerson = new Hashtable<>();
+
+        for (int[] edge : trust){
+            trusters.add(edge[0]);
+            if (!trusteesPerPerson.containsKey(edge[1]))
+                trusteesPerPerson.put(edge[1], 0);
+            trusteesPerPerson.put(edge[1], trusteesPerPerson.get(edge[1]) + 1);
+        }
+
+        int firstSolution = -1;
+        for (int i = 1; i <= peopleInTown; i++){
+            if (!trusters.contains(i)
+                    && trusteesPerPerson.containsKey(i)
+                    && trusteesPerPerson.get(i) == peopleInTown - 1){
                 if (firstSolution != -1)
                     return -1;
                 firstSolution = i;
             }
+        }
+        return firstSolution; */
 
+
+        // whether each person N at index N - 1 trusts someone
+        // if people were named instead of ordered, this could be a HashSet
+        // in order to achieve O(1) performance.
+        boolean trustful[] = new boolean[peopleInTown];
+
+        // index personID -> value count(otherPersonIds who trust personID)
+        // if people were named instead of ordered, this could be a Hashtable
+        // in order to achieve O(1) performance
+        int trustCount[] = new int [peopleInTown];
+
+        for (int[] edge : trust){
+            trustful[edge[0] - 1] = true;
+            ++trustCount[edge[1] - 1];
+        }
+
+        int firstSolution = -1;
+        for (int i = 0; i < peopleInTown; i++){
+            if (!trustful[i] && trustCount[i] == peopleInTown - 1){
+                if (firstSolution != -1) return -1;
+                firstSolution = i + 1;
+            }
         }
         return firstSolution;
     }
