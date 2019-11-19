d8f27db12d6f792ba6e12ea1540efb2662146b30
Levin Weinstein <weinsteinlevin@gmail.com>
2019-11-18 21:10:59 -0800

    Add Adjacency Matrix Graph
    Add my graph. This is poorly encapsulated and needs to be moved around. A lot of my use case is hiding in this.

---
+++ AdjacencyMatrixGraph.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AdjacencyMatrixGraph {
+
+    private int[][] adjacencyMatrix;
+
+    public AdjacencyMatrixGraph(int peopleInTown, int [][] trust){
+        adjacencyMatrix = new int[peopleInTown][peopleInTown];
+
+        for (int[] edge : trust){
+            adjacencyMatrix[edge[0]][edge[1]] = 1;
+        }
+    }
+
+    public boolean trustsNoOne(int person){
+        for (int i = 0; i < adjacencyMatrix.length; i++){
+            if (i != person && adjacencyMatrix[person][i] != 0)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean everyoneTrusts(int person){
+        for (int i = 0; i < adjacencyMatrix.length; i++){
+            if (i != person && adjacencyMatrix[i][person] == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public int findJudge(){
+        int firstSolution = -1;
+
+        for (int i = 0; i < adjacencyMatrix.length; i++){
+            if (trustsNoOne(i) && everyoneTrusts(i)){
+                if (firstSolution != -1)
+                    return -1;
+                firstSolution = i;
+            }
+
+        }
+        return firstSolution;
+    }
+}
