262a3f7b8ec4bb868f69050f3b0c31b634565dee
Levin Weinstein <weinsteinlevin@gmail.com>
2019-11-19 09:34:03 -0800

    Update Judge - Array Version

--- Judge.java
+++ Judge.java
@@ -1,16 +1,16 @@
 public class Judge {
 
-    public int findJudge(int peopleInTown, int [][] trust){
+    public int findJudge(int N, int [][] trust){
 
         // whether each person N at index N - 1 trusts someone
         // if people were named instead of ordered, this could be a HashSet
         // in order to achieve O(1) performance.
-        boolean trustful[] = new boolean[peopleInTown];
+        boolean trustful[] = new boolean[N];
 
         // index personID -> value count(otherPersonIds who trust personID)
         // if people were named instead of ordered, this could be a Hashtable
         // in order to achieve O(1) performance
-        int trustCount[] = new int [peopleInTown];
+        int trustCount[] = new int [N];
 
         for (int[] edge : trust){
             trustful[edge[0] - 1] = true;
@@ -18,8 +18,8 @@ public class Judge {
         }
 
         int firstSolution = -1;
-        for (int i = 0; i < peopleInTown; i++){
-            if (!trustful[i] && trustCount[i] == peopleInTown - 1){
+        for (int i = 0; i < N; i++){
+            if (!trustful[i] && trustCount[i] >= N - 1){
                 if (firstSolution != -1) return -1;
                 firstSolution = i + 1;
             }
